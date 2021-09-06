69071d0e79d99c144f2d1e87196182ae43ad9aa5
ganeaflorin <ganeafloman@gmail.com>
2021-09-06 19:18:09 +0300

    navbar styling

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,10 @@
-import { useState } from "react";
 
 function Home() {
-  const [name, setName] = useState("Flo");
-  const handleClick = () => {
-    setName("Flominisomo");
-  };
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{name}</p>
-      <button onClick={handleClick}>Click me</button>
+
+
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,14 @@
+import './navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <h1>FLOG</h1>
-      <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">New Post</a>
+      <div className = "navbar__container">
+        <img className="navbar__img"src="https://www.adaptivewfs.com/wp-content/uploads/2020/07/logo-placeholder-image.png" alt="fnatic-logo"></img>
+        <div className="navbar__links">
+          <a className="navbar__refs" href="/">HOME</a>
+          <a className="navbar__refs" href="/create">NEW BLOG</a>
+          <a className="navbar__refs" href="/delete">DELETE BLOG</a>
+        </div>
       </div>
     </nav>
   );
